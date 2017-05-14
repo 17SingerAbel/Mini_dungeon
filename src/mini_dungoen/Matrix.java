336dde314b446f4909baf4e45c433d3fd3c9c3a1
abelaura <abelsang9683@gmail.com>
2017-05-14 00:03:14 -0400

    Add more test case, correct variable convention.

--- src/mini_dungoen/Matrix.java
+++ src/mini_dungoen/Matrix.java
@@ -2,65 +2,79 @@ package mini_dungoen;
 
 public class Matrix<T> {
   /** The length of the row in the matrix. */
-  private int lenRow;
+  private final int lenRow;
   /** The lenght of the column in the matrix. */
-  private int lenCol;
+  private final int lenCol;
   /** The total number of elements in the matrix. */
   private int size;
   /** The container contains Elements T. */
-  private T[] container;
-  //private int row = 0;
-  //private int column = 0;
- 
+  private Object[] container;
    
-  @SuppressWarnings("unchecked")
-  public Matrix(int lenRow, int lenCol) {
+  public Matrix(int row, int col) {
     // len is start at 1;
-    if ((lenRow > 0) && (lenCol > 0)) {
-      this.lenRow = lenRow;
-      this.lenCol = lenCol;
-      this.size = lenRow * lenCol;
-      this.container = (T[]) new Object[size];
-    } else {
-      System.out.println(" Please use the Natural number !!!");
+    if (row <= 0) {
+      throw new IndexOutOfBoundsException( " Size of Row must be larger than 0 !!");
     }
+    if (col <= 0) {
+      throw new IndexOutOfBoundsException(" Size of Column must be larger than 0 !!");
+    }
+    this.lenRow = row;
+    this.lenCol = col;
+    this.size = lenRow * lenCol;
+    this.container = new Object[size];
   }
  
   /**
    * Add an item T, at the specific location in matrix, into the container. 
    * Using 0 as the first index of row and column
    * @param item T.
-   * @param row The number of row.
-   * @param column The number of column.
-   * @throws LocationNotDefinedException 
+   * @param rowIndex The index of row. (start from 0)
+   * @param columnIndex The index of column. (start from 0)
+   * @throws IndexOutOfBoundsException 
    */
-  public void setItem(T item, int row, int column) throws IndexOutOfBoundsException {
+  public void setItem(T item, int rowIndex, int columnIndex) throws IndexOutOfBoundsException {
     // len starts at 1, and always lenRow = row(max) + 1, lenCol = col(max) + 1;
-    if ((row < lenRow ) && (column < lenCol)) {
-      int index = lenRow * row + column;
-      this.container[index] = item;
-    } else {
-      throw new IndexOutOfBoundsException();
-    }
+    checkBoundary(rowIndex, columnIndex);
+    int index = lenRow * rowIndex + columnIndex;
+    this.container[index] = item;
   }
   
   /**
    * Get the item T at the specific location in matrix from the container.
    * Using 0 as the first index of row and column
-   * @param row
-   * @param column
-   * @return
-   * @throws LocationNotDefinedException
+   * @param rowIndex The index of row. (start from 0)
+   * @param columnIndex The index of column. (start from 0)
+   * @return T An item.
+   * @throws IndexOutOfBoundsException
    */
-  public T getItem(int row, int column) throws IndexOutOfBoundsException {
+  public T getItem(int rowIndex, int columnIndex) throws IndexOutOfBoundsException {
     // len starts at 1, and always lenRow = row(max) + 1, lenCol = col(max) + 1;
-    if ((row < lenRow) && (column < lenCol)) {
-      int index = lenRow * row + column;
-      T result = this.container[index];
+    checkBoundary(rowIndex, columnIndex);
+    int index = rowIndex * lenRow + columnIndex;
+    T result = (T) this.container[index];
     return result;
-    } else {
-    throw new IndexOutOfBoundsException();
+  }
+  
+  /**
+   * Check the index is always less than the lenght of the row and column.
+   * @param rowIndex The index of row, starts with 0.
+   * @param columnIndex The index of column, starts with 0.
+   */
+  public void checkBoundary(int rowIndex, int columnIndex) {
+    if ((rowIndex >= lenRow) || (rowIndex < 0)) {
+      throw new IndexOutOfBoundsException(" rowIndex must be less than the length of Row !!");
     }
+    if ((columnIndex >= lenCol) || (columnIndex < 0)) {
+      throw new IndexOutOfBoundsException(" columnIndex must be less than the length of Column !!");
+    }
+  }
+  
+  public int getLenRow() {
+    return this.lenRow;
+  }
+  
+  public int getLenCol() {
+    return this.lenCol;
   }
   
 }
--- src/mini_dungoen/MatrixTest.java
+++ src/mini_dungoen/MatrixTest.java
@@ -12,12 +12,25 @@ public class MatrixTest {
   
   @Test
   public void testConstructor() {
-    Matrix<Integer> matrix1 = new Matrix<Integer>(3, 3);
-    Matrix<String> matrix2 = new Matrix<String>(2, 3);
+    int row = 3;
+    int col = 3;
+    Matrix<Integer> matrix = new Matrix<Integer>(row, col);
+    assertEquals(matrix.getLenCol(), 3);
+    assertEquals(matrix.getLenRow(), 3);
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class) 
+  public void testConstructorWithZeroRow() {
+    Matrix<String> matrix = new Matrix<String>(0, 3);
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class) 
+  public void testConstructorWithZeroColumn() {
+    Matrix<String> matrix = new Matrix<String>(3, 0);
   }
   
   @Test
-  public void testAddItemWithInt() {
+  public void testSetAndGetItemWithInt() {
     Matrix<Integer> matrix1 = new Matrix<Integer>(3, 3);
     matrix1.setItem(5, 2, 2);
     assertEquals(matrix1.getItem(2,2),Integer.valueOf(5)); 
@@ -26,7 +39,7 @@ public class MatrixTest {
   }
    
   @Test
-  public void testAddItemWithString() {
+  public void testSetAndGetItemWithString() {
     Matrix<String> matrix2 = new Matrix<String>(2, 3);
     matrix2.setItem("D", 1, 1);
     assertEquals(matrix2.getItem(1,1), "D");
@@ -34,6 +47,30 @@ public class MatrixTest {
     assertEquals(matrix2.getItem(0,1), "A");
     matrix2.setItem("f", 1, 0);
     assertEquals(matrix2.getItem(1,0), "f");
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testSetAndGetItemWithLargerRowIndex() {
+    Matrix<Integer> matrix1 = new Matrix<Integer>(3, 3);
+    matrix1.setItem(3, 4, 2);
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testSetAndGetItemWithLargerColumnIndex() {
+    Matrix<Integer> matrix1 = new Matrix<Integer>(4, 3);
+    matrix1.setItem(333, 2, 4);
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testSetAndGetItemWithNegativeRowIndex() {
+    Matrix<String> matrix2 = new Matrix<String>(3,5);
+    matrix2.setItem("ASDF", -2, 3);
+  }
+  
+  @Test(expected = IndexOutOfBoundsException.class)
+  public void testSetAndGetItemWithNegativeColIndex() {
+    Matrix<String> matrix2 = new Matrix<String>(3,5);
+    matrix2.setItem("ASDF", 1, -4);
   }
   
 }
